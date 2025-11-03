8ed09b2f35c2ab4e8ceeea6c43f79e6e1e9aa1ea
Tasin-Tausif <tasin@magnetismtech.com>
2025-11-03 17:26:10 +0600

    Wrong count

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,8 +5,13 @@ import Keyboard from './Keyboard.jsx'
 import { useState } from 'react'
 
 export default function () {
+    // State Value
     const [currentWord, setCurrentWord] = useState("react")
     const [guessedLetters, setGuessedLetters] = useState([]);
+
+    // Derived Value
+    const wrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(letter)).length
+    console.log(wrongGuessCount)
 
     return (
         <main>
